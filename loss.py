b6d5b28deee1e5997f9dedad47908a2c78575369
lcy80366872 <2428028326@qq.com>
2023-12-13 12:42:12 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,53 +1,8 @@
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import cv2
 import numpy as np
-from torch.nn import functional as F
 
-
-class BoundaryBCELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, edge, target, boundary):
-        # edge = edge.squeeze(dim=1)
-        mask = target
-        pos = torch.count_nonzero(boundary)
-        num = mask.numel()
-
-        neg_weight = pos / num+0.0
-        pos_weight = 1-neg_weight
-        weight = torch.zeros_like(boundary)
-        weight[boundary!=0.0] = pos_weight
-        weight[boundary==0.0] = neg_weight
-        loss = F.binary_cross_entropy(edge, boundary, weight, reduction='sum') / num
-        return loss
-
-
-class DualTaskLoss(nn.Module):
-    def __init__(self, threshold=0.8):
-        super().__init__()
-        self.threshold = threshold
-
-    def forward(self, seg, edge, target):
-        # edge = edge.squeeze(dim=1)
-        logit = F.binary_cross_entropy(seg, target,reduction='none')
-        mask = target
-        num = (mask[edge > self.threshold]).numel()
-        loss = (logit[edge > self.threshold].sum()) / num
-        return loss
-def dice_loss(predict, target):
-    smooth = 1
-    p = 2
-    valid_mask = torch.ones_like(target)
-    predict = predict.contiguous().view(predict.shape[0], -1)
-    target = target.contiguous().view(target.shape[0], -1)
-    valid_mask = valid_mask.contiguous().view(valid_mask.shape[0], -1)
-    num = torch.sum(torch.mul(predict, target) * valid_mask, dim=1) * 2 + smooth
-    den = torch.sum((predict.pow(p) + target.pow(p)) * valid_mask, dim=1) + smooth
-    loss = 1 - num / den
-    return loss.mean()
 
 class SegmentationLosses(object):
     def __init__(self, weight=None, size_average=True, batch_average=True, ignore_index=255, cuda=False):
@@ -63,14 +18,14 @@ class SegmentationLosses(object):
             return self.CrossEntropyLoss
         elif mode == 'focal':
             return self.FocalLoss
-        elif mode =='con_ce':
+        elif mode == 'con_ce':
             return self.ConLoss
         else:
             raise NotImplementedError
 
     def CrossEntropyLoss(self, logit, target):
         n, c, h, w = logit.size()
-        criterion = nn.CrossEntropyLoss(weight=self.weight, #ignore_index=self.ignore_index,
+        criterion = nn.CrossEntropyLoss(weight=self.weight,  # ignore_index=self.ignore_index,
                                         size_average=self.size_average)
         if self.cuda:
             criterion = criterion.cuda()
@@ -91,7 +46,7 @@ class SegmentationLosses(object):
 
     def FocalLoss(self, logit, target, gamma=2, alpha=0.25):
         n, c, h, w = logit.size()
-        criterion = nn.CrossEntropyLoss(weight=self.weight, #ignore_index=self.ignore_index,
+        criterion = nn.CrossEntropyLoss(weight=self.weight,  # ignore_index=self.ignore_index,
                                         size_average=self.size_average)
         if self.cuda:
             criterion = criterion.cuda()
@@ -107,6 +62,7 @@ class SegmentationLosses(object):
 
         return loss
 
+
 class dice_bce_loss(nn.Module):
     def __init__(self, batch=True):
         super(dice_bce_loss, self).__init__()
@@ -114,7 +70,7 @@ class dice_bce_loss(nn.Module):
         self.bce_loss = nn.BCELoss()
 
     def soft_dice_coeff(self, y_true, y_pred):
-        smooth = 1.0 
+        smooth = 1.0
         if self.batch:
             i = torch.sum(y_true)
             j = torch.sum(y_pred)
@@ -132,41 +88,27 @@ class dice_bce_loss(nn.Module):
 
     def resize(self, y_true, h, w):
         b = y_true.shape[0]
-        y = np.zeros((b, h,w ,y_true.shape[1]))
-        
+        y = np.zeros((b, h, w, y_true.shape[1]))
+
         y_true = np.array(y_true.cpu())
         for id in range(b):
-            y1 = y_true[id,:,:,:].transpose(1,2,0)
+            y1 = y_true[id, :, :, :].transpose(1, 2, 0)
+            #             print('y1:',y1.shape)
             a = cv2.resize(y1, (h, w))
             if a.ndim == 2:
-                a=np.expand_dims(a,axis=-1)
-            y[id, :, :, :]=a
-        y=y.transpose(0,3,1,2)
+                a = np.expand_dims(a, axis=-1)
+            #             print('a:',a.shape)
+            #             print('y:',y.shape)
+            y[id, :, :, :] = a
+        y = y.transpose(0, 3, 1, 2)
         return torch.Tensor(y)
-        
-    def __call__(self, y_true, y_pred,edge_map):
+
+    def __call__(self, y_true, y_pred):
         # the ground_truth map is resized to the resolution of the predicted map during training
         if y_true.shape[2] != y_pred.shape[2] or y_true.shape[3] != y_pred.shape[3]:
-            
             y_true = self.resize(y_true, y_pred.shape[2], y_pred.shape[3]).cuda()
-#             print(y_true.shape)
-        mask = y_true.clone()[:, 0, :, :].unsqueeze(1)
-        edge = y_true.clone()[:, 1, :, :].unsqueeze(1)
-        a = self.bce_loss(y_pred, mask)
-        b = self.soft_dice_loss(mask, y_pred)
-        # edge =F.interpolate(edge, scale_factor=0.25, mode='bilinear', align_corners=False)
-        edge_=edge_map.detach().clone()
-        edge[edge>0]=1
-        plt.subplot(2,3,1)
-        plt.imshow(edge_.cpu()[0][0].numpy())
-        plt.subplot(2, 3, 2)
-        plt.imshow(edge.cpu()[0][0].numpy())
-        plt.subplot(2, 3, 3)
-        plt.imshow(y_pred.detach().cpu()[0][0].numpy())
-        plt.show()
-        c = BoundaryBCELoss()(edge_map,mask, edge)
-        print('loss:',a+b)
-        print('edge',c)
-        d = DualTaskLoss()(y_pred,edge_map,mask)
-        print('task',d)
-        return a + b + c+ d
+        #             print(y_true.shape)
+
+        a = self.bce_loss(y_pred, y_true)
+        b = self.soft_dice_loss(y_true, y_pred)
+        return a + b
